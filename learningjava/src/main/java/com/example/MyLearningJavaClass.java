ed424ea859246d5546b5965c843c3adf91d788bd
github username <cenkerdemir@yahoo.com>
2016-10-01 13:20:29 -0400

    project files added

---
+++ learningjava/src/main/java/com/example/MyLearningJavaClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyLearningJavaClass {
+
+    public static void main(String[] args) {
+
+        String name = "cenker";
+        int age = 21;
+
+        System.out.println(name + " is not " + age + ".");
+
+    }
+
+}
