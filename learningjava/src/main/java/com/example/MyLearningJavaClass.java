c1519328fbb3aa66e84ea7897d94315d60d951ab
github username <cenkerdemir@yahoo.com>
2016-10-03 19:54:29 -0400

    Added a for loop, All is well...

--- learningjava/src/main/java/com/example/MyLearningJavaClass.java
+++ learningjava/src/main/java/com/example/MyLearningJavaClass.java
@@ -2,17 +2,27 @@ package com.example;
 
 public class MyLearningJavaClass {
 
+    static String newName = "Cenker Demir";
+
     public static void main(String[] args) {
 
         String name = "cenker";
-        int age = 23;
+        int age = 19;
 
         if(age >= 21) {
             System.out.println(name + ", you are old enough to drink");
+            myFunc();
         }
         else {
             System.out.println(name + ", you are only " + age + " years old.");
         }
+
     }
 
+    public static void myFunc() {
+
+        for (int i = 0; i < 50 ; i++) {
+            System.out.println(i+1 + " " + newName);
+        }
+    }
 }
