227370a05fe4d454efae984cff3549e81e066b5d
github username <cenkerdemir@yahoo.com>
2016-10-03 21:28:45 -0400

    array and functions

--- learningjava/src/main/java/com/example/MyLearningJavaClass.java
+++ learningjava/src/main/java/com/example/MyLearningJavaClass.java
@@ -2,13 +2,15 @@ package com.example;
 
 public class MyLearningJavaClass {
 
-    static String newName = "Cenker Demir";
+    static String newName = "A crying moose";
 
     //main function
     public static void main(String[] args) {
-
         String name = "cenker";
-        int age = 39;
+        int age = 19;
+        double pi = 3.14;
+        boolean iAmCool = true;
+        String[] names = new String[]{"cenker", "helen", "new york", "new jersey"};
 
         if(age >= 21) {
             System.out.println(name + ", you are old enough to drink");
@@ -17,6 +19,12 @@ public class MyLearningJavaClass {
         else {
             System.out.println(name + ", you are only " + age + " years old.");
         }
+
+        //printing out the elements of names array
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 
     //helper function that helps nothing really, just uses a for loop
@@ -25,5 +33,14 @@ public class MyLearningJavaClass {
         for (int i = 0; i < 50 ; i++) {
             System.out.println(i+1 + " " + newName);
         }
+
+        if (newName == "A laughing Cow") {
+            System.out.println("No way!!!");
+        }
+        else {
+            System.out.println("Yes way!!!");
+        }
     }
+
+
 }
