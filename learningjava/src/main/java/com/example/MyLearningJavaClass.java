21c2967544103346ead7e8311b716f72739f28ff
github username <cenkerdemir@yahoo.com>
2016-10-03 23:18:54 -0400

    classes added. java intro completed. moving on to making an app now

--- learningjava/src/main/java/com/example/MyLearningJavaClass.java
+++ learningjava/src/main/java/com/example/MyLearningJavaClass.java
@@ -2,15 +2,25 @@ package com.example;
 
 public class MyLearningJavaClass {
 
-    static String newName = "A crying moose";
+    static private String newName = "A crying moose";
 
     //main function
     public static void main(String[] args) {
         String name = "cenker";
         int age = 19;
-        double pi = 3.14;
-        boolean iAmCool = true;
-        String[] names = new String[]{"cenker", "helen", "new york", "new jersey"};
+        //double pi = 3.14;
+        //boolean iAmCool = true;
+       // String[] names = new String[]{"cenker", "helen", "new york", "new jersey"};
+        //double[] myDoubles = new double[]{2.1, 3.2, 5.4365};
+        int[] numbers = new int[]{3, 4, 5, 34, 76, 456, 2, 7, 4, 6, 5, 3};
+        int totalNumbers = 0;
+
+        if (numbers.length >= 10) {
+            System.out.println("BIG! Numbers array has more than 10 elements");
+        }
+        else {
+            System.out.println("numbers array has less than 10 elements");
+        }
 
         if(age >= 21) {
             System.out.println(name + ", you are old enough to drink");
@@ -20,11 +30,43 @@ public class MyLearningJavaClass {
             System.out.println(name + ", you are only " + age + " years old.");
         }
 
-        //printing out the elements of names array
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
+        //add the elements of numbers array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+            totalNumbers += numbers[i];
         }
+        System.out.println("total number is: " + totalNumbers);
 
+        System.out.println(fullName("Cenker", "Demir"));
+
+        //Classes
+        Dog myPup = new Dog();
+        myPup.name = "Fido";
+        myPup.age = 5;
+        myPup.furColor = "Brown";
+
+        Dog anotherPup = new Dog();
+        anotherPup.name = "Haley";
+        anotherPup.age = 13;
+        anotherPup.furColor = "White";
+
+        myPup.tellMeAboutTheDog();
+        anotherPup.tellMeAboutTheDog();
+
+        Poodle myPoodle = new Poodle();
+        myPoodle.name = "frigging thing";
+        myPoodle.age = 23;
+        myPoodle.curlyFactor = 0.3;
+
+        System.out.println(myPoodle.curlyFactor);
+
+        //a new chair object
+        Chair myChair = new Chair();
+        myChair.shape = "classic";
+        myChair.color = "light brown";
+        myChair.heightInFeet = 4;
+
+        myChair.chairDescription();
     }
 
     //helper function that helps nothing really, just uses a for loop
@@ -33,7 +75,6 @@ public class MyLearningJavaClass {
         for (int i = 0; i < 50 ; i++) {
             System.out.println(i+1 + " " + newName);
         }
-
         if (newName == "A laughing Cow") {
             System.out.println("No way!!!");
         }
@@ -42,5 +83,35 @@ public class MyLearningJavaClass {
         }
     }
 
+    //function to return full name if the first and last name were given
+    public static String fullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+}
 
+//new class
+class Dog {
+    String name = "";
+    int age = 0;
+    String furColor = "";
+
+    void tellMeAboutTheDog() {
+        System.out.println("The dog's name is " + name + ". It is " + age + " years old " + "and its fur color is " + furColor);
+    }
+}
+
+// extending the Dog class
+class Poodle extends Dog {
+    double curlyFactor = 0.0;
+}
+
+//a new class
+class Chair {
+    String shape = "";
+    String color = "";
+    int heightInFeet = 3;
+
+    void chairDescription() {
+        System.out.println("The chair's shape is " + shape + ", its color is " + color + ", and its height is " + heightInFeet + " feet.");
+    }
 }
